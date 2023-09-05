cd62c67ff22db0742853e9b8e1ca7426ad3ba458
Jszir <123523194+Jszir@users.noreply.github.com>
2023-09-05 10:51:32 -0500

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+ingredients.forEach((ingredient) => {
+const list = document.createElement('li'); 
+list.textContent = ingredient ;
+list.classList.add('item');
+const ingredientList = document.getElementById('ingredients');
+ingredientList.append(list);
+
+})
