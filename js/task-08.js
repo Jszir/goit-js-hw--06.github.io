0307d85b95391d1095132a42c666a3152b77472d
Jszir <123523194+Jszir@users.noreply.github.com>
2023-09-05 10:52:47 -0500

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
+
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            const emailInput = form.elements.email;
+            const passwordInput = form.elements.password;
+
+            if (emailInput.value === '' || passwordInput.value === '') {
+                alert('Todos los campos deben ser completados');
+            } else {
+                const formData = {
+                    email: emailInput.value,
+                    password: passwordInput.value
+                };
+
+                console.log(formData);
+                form.reset();
+            }
+        });
 
